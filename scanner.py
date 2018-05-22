91ea38db96cc0bc5e06d40f1e20985ddba6357c2
J. Victor Elliott <wyrdspoon@gmail.com>
2018-05-22 13:41:12 -0500

    added

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 
+if len(sys.argv) < 2: print("Usage: scanner.py REMOTE_IP")
+
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 except socket.error:
