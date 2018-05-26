1ff654fd9fa558eff3020c28f48e4d68302229a4
J. Victor Elliott <wyrdspoon@gmail.com>
2018-05-26 15:19:24 -0500

    using Scapy now

--- scanner.py
+++ scanner.py
@@ -1,10 +1,12 @@
-import socket
+from scapy.all import sr1,IP,TCP
 import sys
 
-if len(sys.argv) < 2: print("Usage: scanner.py REMOTE_IP")
+if len(sys.argv) < 2:
+	print("Usage: scanner.py REMOTE_IP")
+	sys.exit(1)
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-except socket.error:
-	print('ERROR: Socket could not be created.')
-	sys.exit()
+target = sys.argv[1]
+
+res, unans = sr( IP(dst=target) /TCP(flags="S", dport=(1,1024)) )
+
+res.nsummary( lfilter=lambda (s,r): (r.hashlayer(TCP) and (r.getlayer(TCP).flags & 2)) )
